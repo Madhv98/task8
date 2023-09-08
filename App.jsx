0d6b2bd283a0fdf5542540aeca79ca8a809a0775
Madhav Dhital <mdhital@madhavs-mbp.mynetworksettings.com>
2023-09-08 17:04:28 -0400

    Layouts in css

---
+++ App.jsx
@@ -0,0 +1,21 @@
+// Creat app component 
+
+import Header from "./head/header"
+import Home from "./home/home"
+import Footer from "./footer/footer"
+
+function App() {
+  return(
+  <>
+ <Header />
+ <Home/> 
+ <Footer/>
+ </>
+ 
+  )
+}
+
+export default App
+
+
+
